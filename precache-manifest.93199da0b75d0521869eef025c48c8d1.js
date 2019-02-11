848f4370d88a97c34fbcb45fa32983eb5aad0db3
foxyg <layantobi@gmail.com>
2019-02-11 16:04:39 +0100

    Updates

---
+++ precache-manifest.93199da0b75d0521869eef025c48c8d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d7b89bb5b1b0925bf14",
+    "url": "/static/js/main.8d7b89bb.chunk.js"
+  },
+  {
+    "revision": "668495c897efb7bfee98",
+    "url": "/static/js/1.668495c8.chunk.js"
+  },
+  {
+    "revision": "8d7b89bb5b1b0925bf14",
+    "url": "/static/css/main.f900a5ee.chunk.css"
+  },
+  {
+    "revision": "668495c897efb7bfee98",
+    "url": "/static/css/1.55bd74d7.chunk.css"
+  },
+  {
+    "revision": "edd73e5d7a68138d0421b09b4bc0bcd5",
+    "url": "/index.html"
+  }
+];
